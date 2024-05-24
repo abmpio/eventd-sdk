401e005a5c29c28c319444c3b594743e062725cf
zcqing <jeseeqing@gmail.com>
2024-05-24 13:13:02 +0800

    add eventd reconnect,disconnect event handler

--- client/client.go
+++ client/client.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/abmpio/abmp/pkg/log"
 	"github.com/abmpio/eventd_sdk/options"
 	"github.com/nats-io/nats.go"
 )
@@ -34,7 +35,10 @@ func SetGlobalClient(c *EventdClient) {
 
 func (c *EventdClient) Connect() error {
 	// Connect Options.
-	opts := []nats.Option{nats.Name("eventd publisher")}
+	opts := []nats.Option{
+		nats.Name("eventd publisher"),
+		nats.MaxReconnects(-1),
+	}
 
 	nkeyFilePath := options.GetOptions().NKeyFile
 	// Use Nkey authentication.
@@ -55,6 +59,20 @@ func (c *EventdClient) Connect() error {
 	if err != nil {
 		return err
 	}
+	nc.SetErrorHandler(func(c *nats.Conn, s *nats.Subscription, err error) {
+		log.Logger.Warn(fmt.Sprintf("eventd的订阅出现异常,topic:%s,err:%s",
+			s.Subject, err.Error()))
+	})
+	nc.SetDisconnectHandler(func(c *nats.Conn) {
+		clientId, _ := c.GetClientID()
+		log.Logger.Info(fmt.Sprintf("disconnect from eventd server,clientId:%d",
+			clientId))
+	})
+	nc.SetReconnectHandler(func(c *nats.Conn) {
+		clientId, _ := c.GetClientID()
+		log.Logger.Info(fmt.Sprintf("reconnect from eventd server,clientId:%d",
+			clientId))
+	})
 	c._nc = nc
 	return nil
 }
@@ -82,6 +100,16 @@ func Publish(topic string, v interface{}) error {
 		return fmt.Errorf("序列化v时出现异常,err:%s", err.Error())
 	}
 	return _client._nc.Publish(topic, data)
+}
+
+// 重连回调
+func OnReconnected(fn func()) {
+	if err := assetClientInit(); err != nil {
+		return
+	}
+	_client._nc.SetReconnectHandler(func(c *nats.Conn) {
+		fn()
+	})
 }
 
 func Subscribe(topic string, cb nats.MsgHandler) (*nats.Subscription, error) {
